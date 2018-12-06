785706f4d3e6580222772832e8c81695eff1e7e9
o_0 <43157869+danger-academy@users.noreply.github.com>
2018-12-06 14:46:28 -0500

    add for-loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,22 +18,36 @@ $(document).ready(function(){
    
 
     //on button click
-  $(document).on('click', '.expression', function() {
+    $(document).on('click', '.expression', function() {
 
-    var western = $(this).html(); 
-    console.log(western);
+        var western = $(this).html(); 
+            console.log(western);
     
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + western + "&api_key=dc6zaTOxFJmzC&limit=10";
-        // console.log(queryURL);
-        $.ajax({url: queryURL, method: 'GET'})
-        .done(function(response) {
-            // grabs the data
-            var results = response.data;
-            // console.log(results);
-                //empties the div before adding more gifs
-                $('#expressView').empty();
-                    //loops through the data
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + western + "&api_key=dc6zaTOxFJmzC&limit=10";
+            console.log(queryURL);
 
+            $.ajax({url: queryURL, method: 'GET'})
+            .done(function(response) {
+                // grabs the data
+                var results = response.data;
+                    console.log(results);
+
+                    //empties the div before adding more gifs
+                    $('#expressView').empty();
+
+                        //loops through the data
+                        for ( var j=0; j < results.length; j++) {
+                            var imageDiv = $('<div>');
+                            var imageView = results[j].images.fixed_height.url;
+                            var still = results[j].images.fixed_height_still.url;
+                                console.log(imageView);  
+                            var expressImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+                            expressImage.attr('data-state', 'still');
+
+                            $('#expressView').prepend(expressImage);
+                            expressImage.on('click', playGif);
+                        } //for loop
+            }); // done response
                     
 
 

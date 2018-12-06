b697ae42ef86a76749bbd904b8deae5be47eda4e
o_0 <43157869+danger-academy@users.noreply.github.com>
2018-12-06 14:09:07 -0500

    intial js with pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,44 @@
+$(document).ready(function(){
+
+  //create topic array buttons
+    function buttonExpress(){
+        $('#buttonsView').empty();
+        
+        for ( var i=0; i < topics.length; i++) {
+            //create all buttons
+            var a = $('<button>');
+            a.addClass('expression');
+            a.attr('data-name', topics[i]);
+            a.text(topics[i]);
+            $('#buttonsView').append(a);
+        }
+    }    
+    buttonExpress();
+   
+
+    //on button click
+    $(document).on('click', '.expression', function() {
+
+        //console.log(queryURL)
+            
+            //grabs the data
+            
+                //console.log(results)
+
+                    //empties the div before adding more gifs
+                
+                        //loops through the data
+        
+                        
+                    
+               
+
+
+       
+
+
+//adding new button
+
+
+});  //document ready
 
